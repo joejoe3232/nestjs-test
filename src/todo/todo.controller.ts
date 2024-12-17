916e5d4b9c0a300b4e7e54fa9f9889b22099ebd0
JF\Jim <Jim@iamjf.com>
2024-12-17 17:37:57 +0800

    checkin 2

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,15 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param , Query} from '@nestjs/common';
 
 @Controller('todos')
 export class TodoController {
-  @Get('/examples')
-  getExample() {
-    return [
+  //http://localhost:3000/todos/1
+  @Get(':id')
+  get(@Param('id') id: string) {
+    return {
+      id,
+      title: `Title ${id}`,
+      description: ''
+    };
+  }
+
+  //http://localhost:3000/todos?limit=1
+  @Get()
+  getList(
+    @Query('limit') limit: number = 30,
+    @Query('skip') skip: number = 0
+  ) {
+    const list = [
       {
         id: 1,
-        title: 'Example 1',
+        title: 'Title 1',
+        description: ''
+      },
+      {
+        id: 2,
+        title: 'Title 2',
         description: ''
       }
     ];
+
+    return list.slice(skip, limit);
   }
+  
 }
